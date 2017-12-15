f08f1e06efc78263c50ced76127486b035f2da96
Thadeus Ajayi <Thadeus Ajayi>
2017-12-15 22:10:05 +0100

    Fully functional application

--- mongo.sh
+++
@@ -1,1 +0,0 @@
-docker run --name alcapp -p 27017:27017 mongo
--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
+##Uncomment pull node and pull mongo if already done on docker
+
 #docker pull node : boron
 
 #docker pull mongo
 
-#./mongo.sh
-
-docker build -t thadeus/alcapp .
+docker run --name alcapp -p 27017:27017 mongo & docker build -t thadeus/alcapp .
 
 docker run -p 3000:3000 thadeus/alcapp
