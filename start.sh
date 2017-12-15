c876fd3251ed91c8529c9e1da46d98e242a7e163
Thadeus Ajayi <Thadeus Ajayi>
2017-12-15 22:19:20 +0100

    Remove comment from docker pull node and docker pull mongo

--- start.sh
+++ start.sh
@@ -1,8 +1,6 @@
-##Uncomment pull node and pull mongo if already done on docker
+docker pull node : boron
 
-#docker pull node : boron
-
-#docker pull mongo
+docker pull mongo
 
 docker run --name alcapp -p 27017:27017 mongo & docker build -t thadeus/alcapp .
 
